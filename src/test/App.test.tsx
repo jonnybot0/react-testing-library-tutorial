1466ec45a0175003baf2f8c49f943a7e0e835c5c
Jonny Carter <jcarter@adaptavist.com>
2022-02-18 16:16:22 -0600

    Event firing test

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,13 +1,25 @@
 import * as React from 'react'
-import { render, screen } from '@testing-library/react'
+import {fireEvent, render, screen} from '@testing-library/react'
 import App from "../main/App"
 
 describe('App', () => {
     test('renders App component', async () => {
-        render(<App />);
+        render(<App />)
 
         expect(screen.queryByText(/Signed in as/)).toBeNull()
 
         expect(await screen.findByText(/Signed in as/)).toBeInTheDocument()
-    });
-});
+    })
+
+    test('event firing', () => {
+        render(<App />)
+
+        expect(screen.queryByText(/Searches for JavaScript/)).toBeNull()
+
+        fireEvent.change(screen.getByRole('textbox'), {
+            target: { value: 'JavaScript' },
+        })
+
+        expect(screen.getByText(/Searches for JavaScript/)).toBeInTheDocument()
+    })
+})
