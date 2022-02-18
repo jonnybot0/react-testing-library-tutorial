9f31c04351c515f0d811ebb5fa0ec328bd76184d
Jonny Carter <jcarter@adaptavist.com>
2022-02-18 13:30:13 -0600

    Make component use advanced React features like hooks

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -1,9 +1,35 @@
 import * as React from 'react'
 
-const title = 'Hello React'
-
 function App() {
-    return <div>{title}</div>
+    const [search, setSearch] = React.useState('');
+
+    function handleChange(event) {
+        setSearch(event.target.value);
+    }
+
+    return (
+        <div>
+            <Search value={search} onChange={handleChange}>
+                Search:
+            </Search>
+
+            <p>Searches for {search ? search : '...'}</p>
+        </div>
+    );
 }
 
-export default App
+function Search({ value, onChange, children }) {
+    return (
+        <div>
+            <label htmlFor="search">{children}</label>
+            <input
+                id="search"
+                type="text"
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    );
+}
+
+export default App;
