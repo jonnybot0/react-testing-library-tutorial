b57654b0ee53a9bd1fcecf3ef83a23066c87825d
Jonny Carter <jcarter@adaptavist.com>
2022-02-18 14:07:46 -0600

    Setup environment for JS only

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+module.exports = {
+  testEnvironment: 'jsdom',
+  // Automatically clear mock calls, instances and results before every test
+  clearMocks: true,
+
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: true,
+
+  // An array of glob patterns indicating a set of files for which coverage information should be collected
+  // collectCoverageFrom: undefined,
+
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+
+  setupFilesAfterEnv: [
+    "<rootDir>/setupTest.js"
+  ],
+
+  // An array of regexp pattern strings used to skip coverage collection
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
+
+  // Indicates which provider should be used to instrument code for coverage
+  coverageProvider: "v8",
+};
