d6f12822b9935591c3c6abeed40e0c32b80a40b2
Jonny Carter <jcarter@adaptavist.com>
2022-02-18 13:28:26 -0600

    Debug feature

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import App from "../main/App"
 
 describe('App', () => {
     test('renders App component', () => {
-        render(<App />)
-    })
-})
+        render(<App />);
+
+        screen.debug();
+    });
+});
